b2b702228f37a6ed121026108baf5694d0fb0d50
GirkovArpa <girkovarpa@protonmail.com>
2021-02-22 18:51:38 -0600

    :bookmark: First commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# SliderCards
+
+Fun flash cards that slide across your screen.  Perfect for memorizing test material, or learning vocabulary in your favorite foreign language!
+
+## Preview
+
+![animated preview](preview.gif)
+
+## Usage
+
+Download the [Sciter.JS](https://github.com/c-smile/sciter-js-sdk/tree/main/bin) application corresponding to your operating system.  Place it in this folder, run it, and enjoy!
+
+Modify [cards.json](cards.json) to suit your needs.
+
+## Todo
+
+- [ ] Replace the graphics, specifically the card faces
+- [ ] Replace the sounds, specifically the correct and incorrect dings
+- [ ] Actually play the sound effects
+- [ ] Add ability to flip card over when you can't remember the answer
+- [ ] Use spaced repetition algorithm
+- [ ] Option to customize frequency of card appearances
+
+## Copyright
+
+This application uses [Sciter Engine](https://sciter.com), © Terra Informatica Software, Inc.
+
---
+++ main.js
@@ -0,0 +1,98 @@
+import { $, $$ } from '@sciter';
+
+const ENTER = 13;
+const SPACE = 32;
+
+const [screen_width, screen_height] = Window.this.screenBox('frame', 'dimension');
+Window.this.width = screen_width;
+Window.this.height = screen_height;
+Window.this.move(0, 0, screen_width, screen_height, true);
+Window.this.on('statechange', () => Window.this.isTopmost = true);
+
+const seconds = function (s) {
+  return new Promise((resolve) => setTimeout(resolve, s * 1000));
+}
+
+const until = function (condition) {
+  return new Promise((resolve) => {
+    setInterval(() => condition() && resolve());
+  })
+}
+
+const shuffle = function(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = ~~(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+async function displayCard(Q, A) {
+  const card = <div class="card" answer={A}>
+    <span class="question">{Q}</span>
+    <input class="answer" novalue="Enter the answer" />
+  </div>;
+
+  $('body').append(card);
+  $('.answer').focus();
+
+  $('.answer').on('keydown', async function (evt) {
+    if (evt.keyCode === SPACE) {
+      this.parentElement.classList.add('flip');
+    }
+    if (evt.keyCode === ENTER) {
+      const { answer } = this.parentElement.attributes;
+      const correct = this.value === answer;
+      if (correct) {
+        onCorrectAnswer();
+      }
+      if (!correct && !this.parentElement.classList.contains('incorrect')) {
+        this.parentElement.classList.add('incorrect', 'incorrect-a');
+        await seconds(0.05);
+        this.parentElement.classList.add('incorrect-b');
+        this.parentElement.classList.remove('incorrect-a');
+        await seconds(0.05);
+        this.parentElement.classList.add('incorrect-a');
+        this.parentElement.classList.remove('incorrect-b');
+        await seconds(0.05);
+        this.parentElement.classList.add('incorrect-b');
+        this.parentElement.classList.remove('incorrect-a');
+        await seconds(0.05);
+        this.parentElement.classList.remove('incorrect-b', 'incorrect');
+      }
+    }
+  });
+
+  await seconds(.5);
+  $('.card').classList.add('a');
+}
+
+async function onCorrectAnswer() {
+  $('.card').classList.add('b');
+  await seconds(1);
+  $('.card').classList.add('c');
+  await seconds(.25);
+  $('.card').classList.add('c-2');
+  await seconds(.25);
+  $('.card').classList.add('d');
+  await seconds(.75);
+  $('.card').remove();
+}
+
+async function main() {
+  const response = await fetch('cards.json');
+  const cards = await response.json();
+  shuffle(cards);
+  let i = 0;
+  while (true) {
+    if (i === cards.length) {
+      i = 0;
+      shuffle(cards);
+    }
+    const card = cards[i++];
+    const { question, answer } = card;
+    displayCard(question, answer);
+    await until(() => $('.card') === null);
+  }
+}
+
+main();
